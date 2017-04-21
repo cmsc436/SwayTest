bf37ffaa3418221afd08392852506eea22b87138
Shubham Patel <patelrsam@gmail.com>
2017-04-21 13:16:28 -0400

    Initial Commit

---
+++ app/src/main/java/cmsc436/umd/edu/sway/SwayMain.java
@@ -0,0 +1,13 @@
+package cmsc436.umd.edu.sway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SwayMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sway_main);
+    }
+}
